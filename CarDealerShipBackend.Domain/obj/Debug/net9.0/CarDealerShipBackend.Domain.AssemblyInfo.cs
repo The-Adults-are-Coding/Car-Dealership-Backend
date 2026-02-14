c9f17224a5e3f5a02302da8c02c918a3804e6685
theHATEM <hatehassel@gmail.com>
2026-02-14 12:26:51 +0300

    Added required packages

--- CarDealerShipBackend.Domain/obj/Debug/net9.0/CarDealerShipBackend.Domain.AssemblyInfo.cs
+++ CarDealerShipBackend.Domain/obj/Debug/net9.0/CarDealerShipBackend.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarDealerShipBackend.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dac0f2236cd4c58485763aad669cc617bdfb5aa2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarDealerShipBackend.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarDealerShipBackend.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
