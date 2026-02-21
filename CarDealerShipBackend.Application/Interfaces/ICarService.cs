822479e9f6f0131f4907032d659e0fa76acfa010
Ali Mrhj <mrhj26ali@gmail.com>
2026-02-21 13:19:04 +0300

    Made CarResponse DTO and refactored CarService to return CarResponse DTO

--- CarDealerShipBackend.Application/Interfaces/ICarService.cs
+++ CarDealerShipBackend.Application/Interfaces/ICarService.cs
@@ -1,11 +1,11 @@
-﻿using CarDealerShipBackend.Domain.Entities;
+﻿using CarDealerShipBackend.Application.DTOs;
 
 namespace CarDealerShipBackend.Application.Interfaces
 {
     public interface ICarService
     {
-        Task<IEnumerable<Car>> GetAllCarsAsync();
-        Task<IEnumerable<Car>> GetFiveLatestCarsAsync();
-        Task<IEnumerable<Car>> GetAdBannerAsync();
+        Task<IEnumerable<CarResponse>> GetAllCarsAsync();
+        Task<IEnumerable<CarResponse>> GetFiveLatestCarsAsync();
+        Task<IEnumerable<CarResponse>> GetAdBannerAsync();
     }
 }
