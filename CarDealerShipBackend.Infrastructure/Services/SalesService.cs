087fbe7570755a45be08cafd3ff4ca3005fb4b8f
Ali Mrhj <mrhj26ali@gmail.com>
2026-02-21 15:02:27 +0300

    Added SalesService, buy car logic, and installment schedule generation

---
+++ CarDealerShipBackend.Infrastructure/Services/SalesService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealerShipBackend.Infrastructure.Services
+{
+    internal class SalesService
+    {
+    }
+}
