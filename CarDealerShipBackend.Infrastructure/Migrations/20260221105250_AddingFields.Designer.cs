8292b258d66cf837186f430cb1fb167c8fbc12c1
theHATEM <hatehassel@gmail.com>
2026-02-21 13:55:52 +0300

    added some fileds in database

--- CarDealerShipBackend.Infrastructure/Migrations/20260221105250_AddingFields.Designer.cs
+++ CarDealerShipBackend.Infrastructure/Migrations/20260221105250_AddingFields.Designer.cs
@@ -4,6 +4,7 @@ using CarDealerShipBackend.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CarDealerShipBackend.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260221105250_AddingFields")]
+    partial class AddingFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
