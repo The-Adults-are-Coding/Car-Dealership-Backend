cc9aac71116df5197a528269265f4996fb2634a6
raneemo28 <raneem2832004@gmail.com>
2026-02-21 13:40:31 +0300

    added Admin service

---
+++ CarDealerShipBackend.Application/Interfaces/ISalesContractServices.cs
@@ -0,0 +1,17 @@
+﻿using CarDealerShipBackend.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealerShipBackend.Application.Interfaces
+{
+    public interface ISalesContractServices
+    {
+        Task<IEnumerable<SalesContract>> GetAllSalesContracts();
+        Task<IEnumerable<SalesContract>> GetUserSalesContracts(int UserId);
+        Task<SalesContract> GetCarSalesContracts(int CarId);
+
+    }
+}
