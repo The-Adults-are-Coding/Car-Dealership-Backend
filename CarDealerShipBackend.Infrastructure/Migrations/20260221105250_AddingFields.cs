d96ca782cc2497dd7357ad38228ac760dea162f8
theHATEM <hatehassel@gmail.com>
2026-02-21 13:55:52 +0300

    added some fileds in database

---
+++ CarDealerShipBackend.Infrastructure/Migrations/20260221105250_AddingFields.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarDealerShipBackend.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageURl",
+                table: "CARS",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "AspNetUsers",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "National_ID",
+                table: "AspNetUsers",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "occupation",
+                table: "AspNetUsers",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageURl",
+                table: "CARS");
+
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "National_ID",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "occupation",
+                table: "AspNetUsers");
+        }
+    }
+}
