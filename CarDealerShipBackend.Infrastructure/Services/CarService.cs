250b9f462e1ec4ef29fc17cca4c7481cc9030c5d
raneemo28 <raneem2832004@gmail.com>
2026-02-21 12:28:16 +0300

    added car controller

---
+++ CarDealerShipBackend.Infrastructure/Services/CarService.cs
@@ -0,0 +1,29 @@
+﻿using CarDealerShipBackend.Application.Interfaces;
+using CarDealerShipBackend.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealerShipBackend.Infrastructure.Services
+{
+    public class CarService : ICarServices
+    {
+        public CarService() { }
+        public Task<IEnumerable<Car>> GetAdBanner()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Car>> GetAllCars()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Car>> GetFiveCars()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
