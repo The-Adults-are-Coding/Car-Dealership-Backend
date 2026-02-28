2490f2f7883747de4c056f2fa605ffe10852163b
MT-div <thaercoding@gmail.com>
2026-02-28 11:38:10 +0300

    finalizing branch

---
+++ CarDealerShipBackend.Application/DTOs/PaginatedResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealerShipBackend.Application.DTOs
+{
+    public class PaginatedResult<T>
+    {
+        public IEnumerable<T> Items { get; set; } = new List<T>();
+        public int TotalCount { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+
+        public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+
+        public bool HasPrevious => PageNumber > 1;
+
+        public bool HasNext => PageNumber < TotalPages;
+    }
+}
