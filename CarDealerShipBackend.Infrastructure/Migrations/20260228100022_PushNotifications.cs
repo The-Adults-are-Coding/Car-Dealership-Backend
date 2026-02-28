e3a766efa591976f537437907ba3bd063982179c
theHATEM <hatehassel@gmail.com>
2026-02-28 13:10:37 +0300

    Fixed push notifications

---
+++ CarDealerShipBackend.Infrastructure/Migrations/20260228100022_PushNotifications.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarDealerShipBackend.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class PushNotifications : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_INSTALLMENT_PAYMENTS_SALES_CONTRACTS_SalesContractContractId",
+                table: "INSTALLMENT_PAYMENTS");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "SalesContractContractId",
+                table: "INSTALLMENT_PAYMENTS",
+                type: "decimal(65,30)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(65,30)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeviceToken",
+                table: "AspNetUsers",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_INSTALLMENT_PAYMENTS_SALES_CONTRACTS_SalesContractContractId",
+                table: "INSTALLMENT_PAYMENTS",
+                column: "SalesContractContractId",
+                principalTable: "SALES_CONTRACTS",
+                principalColumn: "ContractId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_INSTALLMENT_PAYMENTS_SALES_CONTRACTS_SalesContractContractId",
+                table: "INSTALLMENT_PAYMENTS");
+
+            migrationBuilder.DropColumn(
+                name: "DeviceToken",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "SalesContractContractId",
+                table: "INSTALLMENT_PAYMENTS",
+                type: "decimal(65,30)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(65,30)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_INSTALLMENT_PAYMENTS_SALES_CONTRACTS_SalesContractContractId",
+                table: "INSTALLMENT_PAYMENTS",
+                column: "SalesContractContractId",
+                principalTable: "SALES_CONTRACTS",
+                principalColumn: "ContractId");
+        }
+    }
+}
